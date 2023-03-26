1fd859dedff7e5844c2e4216a141e519e45f9e5a
Adrian Brazionis <hello@adrianbrazionis.me>
2023-03-26 16:11:02 -0300

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="about">
+            <h2>About</h2>
+            <p>I am a frontend developer with a real interest in problem-solving and making things simple and automating daily tasks. It`s very important for me to keep learning new things day by day.</p>
+        </div>
+    );
+}
---
+++ src/components/Interests.jsx
@@ -0,0 +1,8 @@
+export default function Interests() {
+    return (
+        <div className="interests">
+            <h2>Interests</h2>
+            <p>Music fan. Reader. Soccer expert. Basketball Lover. Dreamer Cheff. My dream is travel around the world. Pop/Rock culture makes world a better place.</p>
+        </div>
+    );
+}
